3eaafc4248720a2b648521f189d29d0f41a8a87c
01dnjs <126755691+01dnjs@users.noreply.github.com>
2023-03-18 13:19:58 +0900

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,17 @@
+def exchange(amount):
+    n500=amount//500
+    amount%=500
+    n100=amount//100
+    amount%=100
+    n50=amount//50
+    amount%=50
+    n10=amount//10
+    print('500원 동전의 개수:',n500)
+    print('100원 동전의 개수:',n100)
+    print('50원 동전의 개수:',n50)
+    print('10원 동전의 개수:',n10)
+def get_integer(prompt):
+    total_cost=int(input(prompt))
+    return total_cost
+total_cost=get_integer('동전으로 교환 하고자 하는 금액은? ')
+exchange(total_cost)
